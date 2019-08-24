7e9e0083d9fc69abcafe1021e37e0e78e88cb3fe
Olha Kurkaiedova <olya.kurkaedova@gmail.com>
2019-08-24 18:33:15 +0300

    Update App component with new link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
       <div className="nav-wrapper">
         <a href="/" className="brand-logo">Percolation Simulator</a>
         <ul className="right">
-          <li><a href="https://github.com/olha-kurkaiedova/percolation">View on GitHub</a></li>
+          <li><a href="https://github.com/lotrien/percolation">View on GitHub</a></li>
         </ul>
       </div>
     </nav>
