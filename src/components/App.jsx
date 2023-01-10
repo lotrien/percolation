262e5ebad5d02c5b7d8b222fe2d341fd3c64d4d9
--global <olya.kurkaedova@gmail.com>
2023-01-11 00:49:18 +0200

    Finally, some luck at last mr. Frodo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,72 @@
-import React, { Fragment, useReducer, memo } from 'react';
+import React, { memo, useRef, useState } from 'react';
 
-import rootReducer from '../store/reducer';
-import * as actions from '../store/actions'
+import Percolation from '../utils/Percolation';
 
 import Controls from './Controls';
 import Simulator from './Simulator';
 import Stats from './Stats';
+
+import { random } from '../utils/utils'
+import { INIT_INPUT_STATE } from '../constants';
 
 import '../styles.css'
 
 const MemoStats = memo(Stats);
 
 const App = () => {
-  const [state, dispatch] = useReducer(rootReducer, {
-    simulator: {
-      running: false,
-    },
-    percolation: {},
-    stats: []
-  });
+  const [n, setDimensions] = useState(INIT_INPUT_STATE.n);
+  const [running, setRunning] = useState(false);
+  const [stats, setStats] = useState([]);
+  // forced way to trigger rerender for Simulator component so it will see useRef changes
+  const [_, setSites] = useState(0);
 
-  // ToDo: consider using useContext to cleanup
-  const { stats, percolation, simulator } = state;
+  const percolationModel = useRef(null);
+  const intervalId = useRef(null);
 
-  const createDisjointSet = n => dispatch(actions.createDisjointSet(n));
-  const run = () => dispatch(actions.run());
-  const stop = () => dispatch(actions.stop());
-  const addStats = (n, count) => dispatch(actions.addStats(n, count));
-  const openRandom = () => dispatch(actions.openRandom());
+  const openRandom = () => {
+    while (true) {
+      const max = percolationModel.current._size - 1;
+      const [row, col] = [random(0, max), random(0, max)];
+
+      if (!percolationModel.current.isOpen(row, col)) {
+        percolationModel.current.open(row, col);
+
+        setSites(oldSites => oldSites + 1);
+        break;
+      }
+    }
+  };
+
+  const runSimulation = () => {
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+
+    setRunning(true);
+    percolationModel.current = new Percolation(n);
+
+    intervalId.current = setInterval(() => {
+      openRandom();
+
+      if (percolationModel.current.percolates()) {
+        clearInterval(intervalId.current);
+        setRunning(false);
+        setStats(oldStats => [...oldStats, {
+          n,
+          count: percolationModel.current._openSites,
+          key: oldStats.length + 1,
+        }]);
+      }
+    }, 50);
+  };
+
+  const stopSimulation = () => {
+    clearInterval(intervalId.current);
+    setRunning(false);
+  }
 
   return (
-    <Fragment>
+    <>
       <nav className="blue darken-2">
         <div className="nav-wrapper">
           <a href="/" className="brand-logo">Percolation Simulator</a>
@@ -41,19 +77,17 @@ const App = () => {
       </nav>
       <div className="row">
         <Controls
-          running={simulator.running}
-          pModel={percolation.model}
-          run={run}
-          stop={stop}
-          addStats={addStats}
-          openRandom={openRandom}
-          createDisjointSet={createDisjointSet}
+          n={n}
+          running={running}
+          stopSimulation={stopSimulation}
+          runSimulation={runSimulation}
+          setDimensions={setDimensions}
         >
           <MemoStats stats={stats} />
         </Controls>
-        <Simulator percolation={percolation} n={simulator.n} />
+        <Simulator percolation={percolationModel.current} />
       </div>
-     </Fragment>
+     </>
   );
 };
 
